a50c1d9b9447f752625e094525e7beb41728fbda
Nikolay <Horushko.Nikolay@gmail.com>
2016-12-20 19:51:33 +0300

    init

---
+++ src/test/java/my/nhorushko/requestcmd/pr/ConsoleTest.java
@@ -0,0 +1,19 @@
+package my.nhorushko.requestcmd.pr;
+
+import org.junit.Test;
+
+/**
+ * Created by Nikol on 12/17/2016.
+ */
+public class ConsoleTest {
+    @Test
+    public void read() throws Exception {
+
+    }
+
+    @Test
+    public void write() throws Exception {
+
+    }
+
+}
