5ebf2e83b4e1b762edb6ff66cb2022bb3cf0926d
Nikolay <Horushko.Nikolay@gmail.com>
2016-12-25 12:37:12 +0300

    print commands names with descriptions

--- src/main/java/my/nhorushko/requestcmd/pr/controller/CommandFactory.java
+++ src/main/java/my/nhorushko/requestcmd/pr/controller/CommandFactory.java
@@ -4,16 +4,18 @@ import my.nhorushko.requestcmd.pr.controller.commands.*;
 import my.nhorushko.requestcmd.pr.model.store.Storeable;
 import my.nhorushko.requestcmd.pr.view.View;
 
+import java.util.Arrays;
+
 /**
  * Created by Nikol on 12/17/2016.
  */
 public class CommandFactory {
     private View view;
-    private Command[] availableCommand;
+    private Command[] availableCommands;
 
     public CommandFactory(View view, Storeable store) {
         this.view = view;
-        availableCommand = new Command[]{
+        availableCommands = new Command[]{
                 new AddRequest(view, store),
                 new Exit(view),
                 new ShowRequest(view, store),
@@ -24,7 +26,7 @@ public class CommandFactory {
 
     public Command createCommand(String source) {
         String commandStr = source.split(Command.COMMAND_SEPARATOR)[0];
-        for (Command command : availableCommand) {
+        for (Command command : availableCommands) {
             if (command.getName().equalsIgnoreCase(commandStr)) {
                 command.setup(source);
                 return command;
@@ -33,4 +35,15 @@ public class CommandFactory {
         return new WrongCommand(view);
     }
 
+    public String printAvailableCommands() {
+        StringBuilder result = new StringBuilder();
+        for (Command command : availableCommands) {
+            result.append(command.getName());
+            result.append("\n");
+            result.append("\t");
+            result.append(command.getDescription());
+            result.append("\n");
+        }
+        return result.toString();
+    }
 }
