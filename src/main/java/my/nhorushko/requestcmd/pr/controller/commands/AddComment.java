c53f9fa643178f9e4f93865755f774c27a031113
Nikolay <Horushko.Nikolay@gmail.com>
2016-12-25 15:08:27 +0300

    fix command name

--- src/main/java/my/nhorushko/requestcmd/pr/controller/commands/AddComment.java
+++ src/main/java/my/nhorushko/requestcmd/pr/controller/commands/AddComment.java
@@ -7,11 +7,11 @@ import my.nhorushko.requestcmd.pr.view.View;
  * Created by Nikol on 12/25/2016.
  */
 public class AddComment extends Command {
-    private String NAME = COMMAND_ADD_REQUEST;
+    private String NAME = COMMAND_ADD_COMMENT;
     private String DESCRIPTION = "Add comment to request. Example: '"
             + NAME + COMMAND_SEPARATOR
             + "requestName" + COMMAND_SEPARATOR_TO_STRING
-            + "comment" + COMMAND_SEPARATOR_TO_STRING;
+            + "comment" + COMMAND_SEPARATOR_TO_STRING + "'";
     private final int EXPECTED_COUNT_PARAMETERS = 2;
 
     public AddComment(View view, Storeable store) {
